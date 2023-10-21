0a51e85f40884c60a742ad7c873cfa5f1fd33982
shinsuho1 <suhoshin1816@gmail.com>
2023-10-21 23:20:46 +0900

    메인페이지 스크롤 이벤트 useeffect수정

--- src/commponents/main/Mainpage.js
+++ src/commponents/main/Mainpage.js
@@ -9,46 +9,6 @@ import { useRef, useEffect } from "react";
 
 function Mainpage() {
     const frame = useRef(null);
-    let posArr = [];
-    let eventOnce = true;
-
-    const getOffsetTop = () => {
-        const sections = frame.current.parentElement.querySelectorAll(".page");
-        sections.forEach((el) => {
-            posArr.push(el.offsetTop);
-        });
-    };
-
-    const activation = () => {
-        const sections = frame.current.parentElement.querySelectorAll(".page");
-        const scroll = window.scrollY;
-        const base = -window.innerHeight / 3;
-        sections.forEach((el, index) => {
-            if (scroll >= posArr[index] + base) {
-                sections[index].classList.add("on");
-            }
-            if (scroll >= posArr[3] + base) {
-                if (eventOnce) {
-                    eventOnce = false;
-                    const data_lable = frame.current.querySelectorAll(".table");
-                    data_lable.forEach((el, index) => {
-                        let numElement = el.querySelector(".num");
-                        let num = parseFloat(numElement.innerText);
-                        let count = 0;
-                        let time = 3000 / num;
-                        let interval = setInterval(() => {
-                            if (count === num) {
-                                clearInterval(interval);
-                            } else {
-                                count++;
-                                numElement.innerText = count + "%";
-                            }
-                        }, time);
-                    });
-                }
-            }
-        });
-    };
 
     useEffect(() => {
         setTimeout(() => {
@@ -56,7 +16,49 @@ function Mainpage() {
                 frame.current.parentElement.querySelectorAll(".page");
             sections[0].classList.add("on");
         }, 700);
+        let posArr = [];
+        let eventOnce = true;
 
+        const getOffsetTop = () => {
+            const sections =
+                frame.current.parentElement.querySelectorAll(".page");
+            sections.forEach((el) => {
+                posArr.push(el.offsetTop);
+            });
+        };
+
+        const activation = () => {
+            const sections =
+                frame.current.parentElement.querySelectorAll(".page");
+            const scroll = window.scrollY;
+            const base = -window.innerHeight / 3;
+            sections.forEach((el, index) => {
+                if (scroll >= posArr[index] + base) {
+                    sections[index].classList.add("on");
+                }
+                if (scroll >= posArr[3] + base) {
+                    if (eventOnce) {
+                        eventOnce = false;
+                        const data_lable =
+                            frame.current.querySelectorAll(".table");
+                        data_lable.forEach((el, index) => {
+                            let numElement = el.querySelector(".num");
+                            let num = parseFloat(numElement.innerText);
+                            let count = 0;
+                            let time = 3000 / num;
+                            let interval = setInterval(() => {
+                                if (count === num) {
+                                    clearInterval(interval);
+                                } else {
+                                    count++;
+                                    numElement.innerText = count + "%";
+                                }
+                            }, time);
+                        });
+                    }
+                }
+            });
+        };
         getOffsetTop();
         window.addEventListener("resize", getOffsetTop);
         window.addEventListener("scroll", activation);
@@ -64,7 +66,7 @@ function Mainpage() {
             window.removeEventListener("resize", getOffsetTop);
             window.removeEventListener("scroll", activation);
         };
-    }, [getOffsetTop, activation]);
+    }, [frame]);
 
     return (
         <main ref={frame}>
